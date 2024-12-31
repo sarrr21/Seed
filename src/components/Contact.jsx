ba685cb0027dcc7974a7e16c0a588280ac30c295
sarrr21 <gs9425188@gmail.com>
2024-12-31 22:37:53 +0300

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,22 +3,18 @@ import { Mail, Phone, MapPin, Calendar } from "lucide-react";
 export default function ContactInfo() {
   return (
     <section className="p-4 mt-4 text-white text-sm">
-      {/* Title and List Wrapper */}
+      
       <div className="flex flex-col md:flex-row md:justify-between">
         <h2 className="text-xl font-bold mb-4 md:mb-0">Contact</h2>
-        {/* Contact List */}
         <div className="grid grid-cols-2 gap-4 md:gap-8 md:w-3/4">
-          {/* Email */}
           <a href="mailto:taxiservice@gmail.com" className="flex items-center">
             <Mail className="text-yellow-400 mr-2" />
-            <h1>taxiservice@gmail.com</h1>
+            <h1>taxiser@gmail.com</h1>
           </a>
-          {/* Phone */}
           <a href="tel:+49955542654" className="flex items-center">
             <Phone className="text-yellow-400 mr-2" />
             <h1>+49 95554 2654</h1>
           </a>
-          {/* Location */}
           <a
             href="https://www.google.com/maps/place/Berlin,+Germany"
             target="_blank"
